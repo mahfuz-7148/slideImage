d50a8c07c2a870a1580c4a7b260d349699b069f6
“mahfuz-7148” <mrahman7148@gmail.com>
2024-10-27 13:33:09 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+const slides = document.querySelectorAll('.slides img')
+let slideIndex = 0
+let valid = null
+
+const initialSlider = () => {
+    if (slides.length > 0) {
+        slides[slideIndex].classList.add('displaySlide')
+        valid = setInterval(nextSlide, 2000)
+    }
+}
+
+document.addEventListener('DOMContentLoaded', initialSlider)
+
+const nextSlide = () => {
+    slideIndex++;
+    showSlide(slideIndex);
+};
+
+const showSlide = index => {
+    if (index >= slides.length) {
+        slideIndex = 0
+    } else if (index < 0) {
+        slideIndex = slides.length - 1
+    }
+    
+    slides.forEach(slide => {
+        slide.classList.remove('displaySlide')
+    })
+    slides[slideIndex].classList.add('displaySlide')
+}
+document.getElementById('prev').addEventListener('click', () => {
+    slideIndex--
+    showSlide(slideIndex)
+})
+document.getElementById('next').addEventListener('click', () => {
+    slideIndex++
+    showSlide(slideIndex)
+})
